5602580b7630630cbb100874b10ffb4fd86d7e21
CosmicAnkit <61377034+CosmicAnkit@users.noreply.github.com>
2021-01-12 00:50:32 +0530

    Update QRcode.py

--- QRcode.py
+++ QRcode.py
@@ -1,7 +1,7 @@
 import pyqrcode
 import png
 from pyqrcode import QRCode
-x = "This is ankit maurya"
+x = "Hello world"
 qr = pyqrcode.create(x)
 qr.svg("qr1.svg",scale = 8)
 qr.png("qr.png",scale = 6)
